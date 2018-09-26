e3ddf853abafeb23406bedcdfb576bb1c7785673
r6eve <r6eve179f@gmail.com>
2018-09-26 09:06:23 +0900

    Update the settings of leiningen

--- home_dot/.lein/profiles.clj
+++ home_dot/.lein/profiles.clj
@@ -1,10 +1,12 @@
 {:user {:dependencies [[cljfmt "0.5.7"]
                        [cider/orchard "0.3.0"]
+                       [nrepl "0.4.5"]
+                       [iced-nrepl "0.1.4"]
+                       [cider/cider-nrepl "0.18.0"]
                        [jonase/eastwood "0.2.9"
                         :exclusions [org.clojure/clojure]]]
-        :plugins [[cider/cider-nrepl "0.18.0"]
-                  [jonase/eastwood "0.2.9"]
-                  [refactor-nrepl "2.4.0-SNAPSHOT"]]
+        :plugins [[jonase/eastwood "0.2.9"]
+                  [refactor-nrepl "2.4.0"]]
         :repl-options {:nrepl-middleware
                        [cider.nrepl/wrap-complete
                         cider.nrepl/wrap-debug
@@ -18,4 +20,5 @@
                         cider.nrepl/wrap-spec
                         cider.nrepl/wrap-test
                         cider.nrepl/wrap-trace
-                        cider.nrepl/wrap-undef]}}}
+                        cider.nrepl/wrap-undef
+                        iced.nrepl/wrap-iced]}}}

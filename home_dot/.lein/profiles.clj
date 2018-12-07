37b41b7a01c55b422175a0c74beb62956e5662af
r6eve <r6eve179f@gmail.com>
2018-12-07 11:10:01 +0900

    Add lein-bikeshed

--- home_dot/.lein/profiles.clj
+++ home_dot/.lein/profiles.clj
@@ -1,4 +1,5 @@
 {:user {:dependencies [[cljfmt "0.6.2"]
                        [jonase/eastwood "0.3.3"
                         :exclusions [org.clojure/clojure]]]
-        :plugins [[jonase/eastwood "0.3.3"]]}}
+        :plugins [[jonase/eastwood "0.3.3"]
+                  [lein-bikeshed "0.5.1"]]}}

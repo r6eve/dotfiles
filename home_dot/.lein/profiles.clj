014717fa9fa5619a52bc896caaefe6c945d819c8
r6eve <r6eve179f@gmail.com>
2018-12-07 11:06:54 +0900

    Dump up version of dependencies

--- home_dot/.lein/profiles.clj
+++ home_dot/.lein/profiles.clj
@@ -1,4 +1,4 @@
-{:user {:dependencies [[cljfmt "0.6.1"]
-                       [jonase/eastwood "0.2.9"
+{:user {:dependencies [[cljfmt "0.6.2"]
+                       [jonase/eastwood "0.3.3"
                         :exclusions [org.clojure/clojure]]]
-        :plugins [[jonase/eastwood "0.2.9"]]}}
+        :plugins [[jonase/eastwood "0.3.3"]]}}

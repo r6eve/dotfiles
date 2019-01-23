22b69c85f21684f1936e40e6bbcffd5345a93323
r6eve <r6eve179f@gmail.com>
2019-01-23 19:49:05 +0900

    Add lein-kibit

--- home_dot/.lein/profiles.clj
+++ home_dot/.lein/profiles.clj
@@ -2,4 +2,5 @@
                        [jonase/eastwood "0.3.3"
                         :exclusions [org.clojure/clojure]]]
         :plugins [[jonase/eastwood "0.3.3"]
-                  [lein-bikeshed "0.5.1"]]}}
+                  [lein-bikeshed "0.5.1"]
+                  [lein-kibit "0.1.6"]]}}

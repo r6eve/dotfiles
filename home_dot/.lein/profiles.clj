4b93da749ccd27b666d35843c4fc8dfb77ba6d66
r6eve <r6eve179f@gmail.com>
2019-01-31 15:04:28 +0900

    Add lein-ancient

--- home_dot/.lein/profiles.clj
+++ home_dot/.lein/profiles.clj
@@ -2,5 +2,6 @@
                        [jonase/eastwood "0.3.3"
                         :exclusions [org.clojure/clojure]]]
         :plugins [[jonase/eastwood "0.3.3"]
+                  [lein-ancient "0.6.15"]
                   [lein-bikeshed "0.5.1"]
                   [lein-kibit "0.1.6"]]}}

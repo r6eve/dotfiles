971916f9588f5120a91238cd61439c80ae39e832
r6eve <r6eve179f@gmail.com>
2019-02-14 20:12:24 +0900

    Add lein-cloverage and lein-typed

--- home_dot/.lein/profiles.clj
+++ home_dot/.lein/profiles.clj
@@ -4,4 +4,6 @@
         :plugins [[jonase/eastwood "0.3.3"]
                   [lein-ancient "0.6.15"]
                   [lein-bikeshed "0.5.1"]
-                  [lein-kibit "0.1.6"]]}}
+                  [lein-cloverage "1.0.13"]
+                  [lein-kibit "0.1.6"]
+                  [lein-typed "0.4.6"]]}}

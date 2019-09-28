1a38d1dd09ebef80e9059ed8098af7fc06731c52
r6eve <r6eve179f@gmail.com>
2019-09-29 08:03:32 +0900

    Neaten up

--- ln.sh
+++ ln.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 set -eu
 
-pwd=`pwd`
+pwd=$(pwd)
 
-for x in $(find home_dot -maxdepth 1); do
-  [ "$x" = 'home_dot' ] && continue
-  ln -sf "$pwd/$x" $HOME/
-done
+find home_hot/* -maxdepth 0 -exec ln -sf "$pwd/{}" "$HOME/" \;
 
-for x in $(find .config -maxdepth 1); do
-  [ "$x" = '.config' ] && continue
-  ln -sf "$pwd/$x" $HOME/.config/
-done
+find .config/* -maxdepth 0 -exec ln -sf "$pwd/{}" "$HOME/.config/" \;
 
-ln -sf "$pwd/home_dot/.vim" $HOME/.config/nvim
+ln -sf "$pwd/home_dot/.vim" "$HOME/.config/nvim"

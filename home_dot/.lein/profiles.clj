75b7669cc5fc1d999395048f74d3c4046c06ac52
r6eve <r6eve179f@gmail.com>
2019-11-28 10:21:36 +0900

    Bump up dependencies

--- home_dot/.lein/profiles.clj
+++ home_dot/.lein/profiles.clj
@@ -1,4 +1,4 @@
-{:user {:dependencies [[cljfmt "0.6.4"]
+{:user {:dependencies [[cljfmt "0.6.6"]
                        [com.clojure-goes-fast/clj-async-profiler "0.4.0"]
                        [criterium "0.4.5"]
                        [jonase/eastwood "0.3.6"
@@ -9,7 +9,7 @@
                   [lein-bikeshed "0.5.2"]
                   [lein-cloverage "1.1.2"]
                   [lein-codox "0.10.7"]
-                  [lein-kibit "0.1.7"]
+                  [lein-kibit "0.1.8"]
                   [lein-pprint "1.2.0"]
                   [lein-typed "0.4.6"]]
         :eastwood {:linters [:all]}}}

617f981b454b90c23cf0dba38ce4d1632c55f998
r6eve <r6eve179f@gmail.com>
2020-03-14 10:16:59 +0900

    Update ~/.lein/profiles.clj

--- home_dot/.lein/profiles.clj
+++ home_dot/.lein/profiles.clj
@@ -1,15 +1,15 @@
-{:user {:dependencies [[cljfmt "0.6.6"]
+{:user {:dependencies [[cljfmt "0.6.7"]
                        [com.clojure-goes-fast/clj-async-profiler "0.4.0"]
                        [criterium "0.4.5"]
-                       [jonase/eastwood "0.3.7"
+                       [jonase/eastwood "0.3.10"
                         :exclusions [org.clojure/clojure]]]
-        :plugins [[com.clojure-goes-fast/clj-java-decompiler "0.3.0-SNAPSHOT"]
-                  [jonase/eastwood "0.3.7"]
+        :plugins [[com.clojure-goes-fast/clj-java-decompiler "0.3.0"]
+                  [jonase/eastwood "0.3.10"]
                   [lein-ancient "0.6.15"]
                   [lein-bikeshed "0.5.2"]
                   [lein-cloverage "1.1.2"]
                   [lein-codox "0.10.7"]
                   [lein-kibit "0.1.8"]
-                  [lein-pprint "1.2.0"]
+                  [lein-pprint "1.3.2"]
                   [lein-typed "0.4.6"]]
         :eastwood {:linters [:all]}}}
